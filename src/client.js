9370d0865aecb8c74486597551155cbf176db8d2
Lubomir <lubomir.yudenko@gmail.com>
2020-05-07 22:17:28 +0300

    Fix a couple of protect bugs

--- src/client.js
+++ src/client.js
@@ -9,20 +9,23 @@ const client = new owop.Client({
 async function protect(position, size, onAttempt) {
 	const promises = []
 	const pixelColors = {}
-	for (let x = position[0]; x < position[0] + size[0]; x++) {
+	for (let x = position[0]; x <= position[0] + size[0]; x++) {
 		pixelColors[x] = {}
-		for (let y = position[1]; y < position[1] + size[1]; y++)
+		for (let y = position[1]; y <= position[1] + size[1]; y++)
 			promises.push(
 				client.world.getPixel(x, y).then(val => (pixelColors[x][y] = val))
 			)
 	}
 	Promise.all(promises).then(() => {
+		let forceOff = false
 		client.on("pixelUpdate", pixel => {
 			if (
 				pixel.x >= position[0] &&
 				pixel.x <= position[0] + size[0] &&
 				pixel.y >= position[1] &&
-				pixel.y <= position[1] + size[1]
+				pixel.y <= position[1] + size[1] &&
+				//A technical limitation
+				pixel.id !== client.player.id
 			) {
 				let prevent = false
 				if (onAttempt) prevent = onAttempt(pixel)
